d1708913fe27c3fb14a1271da2df3715fc9a16e1
Pawel Zalewski <pawel.zalewski>
2022-01-10 00:18:55 +0100

    nieudana zmiana

--- ST.h
+++ ST.h
@@ -1,29 +1,35 @@
 struct symrec {
     char *name; /* name of symbol */
-    int offset; /* data offset */
-    struct symrec *next; /* link field */
+    int pos;
+    bool inRegister;
 };
-
 typedef struct symrec symrec;
-symrec *identifier;
-symrec *sym_table = (symrec *)0;
+
+int registers[8];
+int registerCounter = 2;
 
 symrec* putsym (char *sym_name){
     symrec* ptr;
     ptr = (symrec *) malloc (sizeof(symrec));
     ptr->name = (char *) malloc (strlen(sym_name)+1);
     strcpy (ptr->name, sym_name);
-    // ptr->offset = data_location();
-    ptr->next = (struct symrec *)sym_table;
-    sym_table = ptr;
+    ptr->pos = registerCounter;
+    ptr->inRegister = true;
+    registers[registerCounter++] = ptr;
     return ptr;
 }
 
 symrec * getsym (char *sym_name){
     symrec *ptr;
-    for ( ptr = sym_table; ptr != (symrec *) 0; ptr = (symrec *)ptr->next ){
-        if (strcmp (ptr->name,sym_name) == 0)
-            return ptr;
+    for (int i = 2; i < std::min(registerCounter, 8); i++){
+        if (strcmp(registers[i]->name, sym_name) == 0)
+            return registers[i];
     }
     return 0;
+}
+
+void printRegs(){
+    for (int i = 0; i < std::min(registerCounter, 8); i++){
+        printf("register[%d]: name = %s, pos = %d, inRegister = %d\n", i, registers[i]->name, registers[i]->pos, registers[i]->inRegister);
+    }
 }
