4457411a972e1a5491e665c8c15db62cbbeda81d
Pawel Zalewski <pawel.zalewski>
2022-01-13 00:57:51 +0100

    dzielenie i generowanie stałych

--- src/value.cpp
+++ src/value.cpp
@@ -3,6 +3,9 @@ extern Declarations* declarations;
 extern Registers* registers;
 extern Output* output;
 
+#define INC_COMMAND 1
+#define SHIFT_COMMAND 2
+
 void idValue::load(){
     ident->loadAddr();
     output->load(registers->addr);
@@ -10,18 +13,48 @@ void idValue::load(){
 
 void numValue::load(){
     output->reset(registers->acc);
-
-    registers->accVal = 0;
-    if (value > 0) {
-        for (int i = 0; i < value; i++) {
-            output->inc(registers->acc);
-            registers->accVal++;
+    if (std::abs(value) > 10){
+        output->reset('b');
+        output->inc('b');
+        std::stack<int> commands;
+        while (std::abs(value) != 1){
+            if (std::abs(value % 2) == 1)
+                commands.push(INC_COMMAND);
+            commands.push(SHIFT_COMMAND);
+            value /= 2;
         }
-    } 
+        output->reset('a');
+        if (value > 0){
+            output->inc('a');
+            while (!commands.empty()){
+                if (commands.top() == INC_COMMAND)
+                    output->inc('a');
+                else
+                    output->shift('b');
+                commands.pop();
+            }
+        }
+        else {
+            output->dec('a');
+            while (!commands.empty()){
+                if (commands.top() == INC_COMMAND)
+                    output->dec('a');
+                else
+                    output->shift('b');
+                commands.pop();
+            }
+        }
+    }
     else {
-        for (int i = value; i < 0; i++) {
-            output->dec(registers->acc);
-            registers->accVal--;
+        if (value > 0) {
+            for (int i = 0; i < value; i++) {
+                output->inc(registers->acc);
+            }
+        } 
+        else {
+            for (int i = value; i < 0; i++) {
+                output->dec(registers->acc);
+            }
         }
     }
 }
